6a6de79bfef0ad9b06d939b7de4096d44dd4e9bf
steineca <steineca@rose-hulman.edu>
2023-09-21 20:53:41 +0000

    more basic interaction

--- Code_Files/armold.py
+++ Code_Files/armold.py
@@ -15,25 +15,38 @@ class ArmoldBrain:
             brain.recordedMovements[rn] = Recording(rn)
     
     def recordMovement(brain, refreshRate, duration):
+        frame = 0
+        secDone = 0
         if (duration != 0):
-            frame = 0.0
-            while(frame < duration):
+            print()
+            while(frame < refreshRate * duration):
+                if (secDone == refreshRate):
+                    print("\n")
+                    secDone = 0
+                frame += 1
+                secDone += 1
                 print(".", end = "")
                 sys.stdout.flush()
-                frame += 1.0 / refreshRate
                 time.sleep(1.0 / refreshRate)
         else:
+            print("  (Press Q or Ctrl+C to stop)\n")
             try:
                 while True:
                     if keyboard.is_pressed("q"):
                         break
+                    if (secDone == refreshRate):
+                        print("\n")
+                        secDone = 0
+                    frame += 1
+                    secDone += 1
                     print(".", end = "")
                     sys.stdout.flush()
                     time.sleep(1.0 / refreshRate)
             except KeyboardInterrupt:
                 pass
-        print("\n\nCan you describe what you just did?\n")
-        moveName = input()
+        print(f"\n\n{frame} frames memorized.")
+        print("\nCan you describe what you just did?\n")
+        moveName = input("> ")
         print("\nCool! Armold now knows how to " + moveName + ".")
         return
     
@@ -49,6 +62,7 @@ class ArmoldBrain:
 class Recording:
     def __init__(recording, filename):
         recording.timeline = []
+        recording.originalRate = 1
         recording.filename = filename
         recording.readRecordingFromFile(recording.filename)
 
@@ -84,25 +98,31 @@ class Controller:
 brain = ArmoldBrain()
 while True:
     time.sleep(0.25)
-    print("\nTell Armold what to do!\nCommands are: rec, play, live, and quit!\n")
-    command = input()
-    print("\nYou told Armold '" + command + "'")
-    if(command == "rec"):
+    print("\nTell Armold what to do!",
+            "\nCommands are:",
+            "\n- (s) study movement",
+            "\n- (p) perform movement",
+            "\n- (l) mirror live movement",
+            "\n- (m) move servo",
+            "\n- (q) quit\n")
+    command = input("> ")
+    if(command == "s"):
+        print("\nYou told Armold to study your movements.")
         while True:
-            print("\nRate of recording? (x / sec)\n")
+            print("\nRate of recording? (Hz)\n")
             try:
-                rateinput = input()
+                rateinput = input("> ")
                 refreshRate = float(rateinput)
-                if (refreshRate > 0):
+                if (refreshRate > 1):
                     break
                 else:
-                    print("\nThe rate needs to be greater than 0, try again.")
+                    print("\nThe rate needs to be greater than 1, try again.")
             except ValueError:
                 print("\n'" + rateinput + "' isn't a number, try again.")
         while True:
-            print("\nHow long should Armold watch you? (Enter '0' to do so indefinitely)\n")
+            print("\nHow many seconds should Armold watch you? (Enter '0' to do so indefinitely)\n")
             try:
-                durinput = input()
+                durinput = input("> ")
                 duration = float(durinput)
                 if (duration >= 0):
                     break
@@ -110,34 +130,40 @@ while True:
                     print("\nThe duration can't be negative, try again.")
             except ValueError:
                 print("\n'" + durinput + "' isn't a number, try again.")
-        print("\n- Armold is studying your movements!\n")
+        print("\n- Armold is studying your movements!")
         brain.recordMovement(refreshRate, duration)
-    elif(command == "play"):
+    elif(command == "p"):
+        print("\nYou told Armold to perform a movement.")
         while True:
             print("\nWhich movement should Armold repeat?")
-            for rn in brain.recordedMovements.keys():
-                print(rn)
-            moveinput = input()
+            for rn, rec in brain.recordedMovements.items():
+                print(f"- {rn} ({len(rec.timeline)} frames, {len(rec.timeline) * (1.0 / rec.originalRate)} secs at {rec.originalRate} Hz originally)")
+            print()
+            moveinput = input("> ")
             if moveinput in brain.recordedMovements:
                 break
             else:
                 print("\n'" + moveinput + "' isn't a movement Armold has memorized, try again.")
         while True:
-            print("\nRate of playback? (x / sec)\n")
+            print("\nRate of playback? (Hz)\n")
             try:
-                rateinput = input()
+                rateinput = input("> ")
                 refreshRate = float(rateinput)
-                if (refreshRate > 0):
+                if (refreshRate > 1):
                     break
                 else:
-                    print("\nThe rate needs to be greater than 0, try again.")
+                    print("\nThe rate needs to be greater than 1, try again.")
             except ValueError:
                 print("\n'" + rateinput + "' isn't a number, try again.")
-        print("\n- Armold is repeating your movement!")
+        print("\n- Armold is going to " + moveinput + "!")
         brain.playbackMovement(moveinput, refreshRate)
-    elif(command == "live"):
-        print("\n- Armold is following movements!")
-    elif(command == "quit"):
+    elif(command == "l"):
+        print("\nYou told Armold to mirror your movements in real-time.")
+        print("\n- Armold is mirroring your movements!")
+    elif(command == "m"):
+        print("\nYou told Armold to move a servo.")
+        print("\n- Okay.")
+    elif(command == "q"):
         print("\n- Armold says 'Bye!'\n")
         break
     else:
