f2397a00df2b9704e366478feb1eb627318665ca
steineca <steineca@rose-hulman.edu>
2023-09-28 21:44:08 +0000

    basic in/out stuff

--- Code_Files/armold.py
+++ Code_Files/armold.py
@@ -53,13 +53,43 @@ class ArmoldBrain:
         return
     
     def playbackMovement(brain, moveName, playbackRate):
+        print("  (Press Q or Ctrl+C to stop)")
+        print()
+        frame = 0
+        secDone = 0
+        movement = brain.recordedMovements[moveName]
+        recLen = round(len(movement.timeline) * (1.0 / playbackRate), 2)
+        try:
+            while(frame < len(movement.timeline)):
+                if keyboard.is_pressed("q"):
+                    break
+                if (frame % playbackRate == 0):
+                    print(f"{recLen - secDone} seconds left...")
+                    secDone += 1
+                brain.robot.setServos(brain.convertToServoVals(movement.getServosAtTime(frame)))
+                frame += 1
+                time.sleep(1.0 / refreshRate)
+        except KeyboardInterrupt:
+            pass
+        print(f"\nArmold is done performing '{moveName}!'")
         return
 
-    def realtimeMovement(brain):
+    def realtimeMovement(brain, refreshRate):
+        print("  (Press Q or Ctrl+C to stop)")
+        print()
+        try:
+            while(True):
+                if keyboard.is_pressed("q"):
+                    break
+                brain.robot.setServos(brain.convertToServoVals(brain.controller.getSensors()))
+                time.sleep(1.0 / refreshRate)
+        except KeyboardInterrupt:
+            pass
         return
     
     def convertToServoVals(brain, sensorVals):
-        return dict()
+        servoVals = dict()
+        return servoVals
 
 class Recording:
     def __init__(recording, filename):
@@ -164,17 +194,29 @@ while True:
             try:
                 rateinput = input("> ")
                 refreshRate = float(rateinput)
-                if (refreshRate > 1):
+                if (refreshRate >= 1):
                     break
                 else:
-                    print("\nThe rate needs to be greater than 1, try again.")
+                    print("\nThe rate needs to be at least 1.0, try again.")
             except ValueError:
                 print("\n'" + rateinput + "' isn't a number, try again.")
         print("\n- Armold is going to " + moveinput + "!")
         brain.playbackMovement(moveinput, refreshRate)
     elif(command == "l"):
         print("\nYou told Armold to mirror your movements in real-time.")
-        print("\n- Armold is mirroring your movements!")
+        while True:
+            print("\nRate of recording? (Hz)\n")
+            try:
+                rateinput = input("> ")
+                refreshRate = float(rateinput)
+                if (refreshRate > 1):
+                    print("\n- Armold is mirroring your movements!")
+                    brain.realtimeMovement(refreshRate)
+                    break
+                else:
+                    print("\nThe rate needs to be greater than 1, try again.")
+            except ValueError:
+                print("\n'" + rateinput + "' isn't a number, try again.")
     elif(command == "m"):
         print("\nYou told Armold to move a servo.")
         print("\nWhich pin is the servo on?")
