59001f03780986e0a7be60e137325af6569d8ac1
RHIT-steineca <steineca@rose-hulman.edu>
2023-12-13 19:12:27 -0500

    updating start vals

--- Code_Files/robot.py
+++ Code_Files/robot.py
@@ -2,13 +2,13 @@ import os, sys, time, csv
 
 valPath = "//home//pi//"
 fullValPath = os.path.join(valPath, "robovals.txt")
-startVals = {"0":0, "1":0, "2":0, "3":0, "4":0, "5":0, "6":0, "7":0, "8":0, "9":0}
-actualVals = {"0":0, "1":0, "2":0, "3":0, "4":0, "5":0, "6":0, "7":0, "8":0, "9":0}
+startVals = {"0":2500, "1":2500, "2":2500, "3":2500, "4":2500, "5":2500, "6":2500, "7":2500, "8":2500, "9":2500}
+actualVals = {"0":2500, "1":2500, "2":2500, "3":2500, "4":2500, "5":2500, "6":2500, "7":2500, "8":2500, "9":2500}
 targetVals = {"0":2500, "1":2500, "2":2500, "3":2500, "4":2500, "5":2500, "6":2500, "7":2500, "8":2500, "9":2500}
 mapping = {"shoulderCB":"0","shoulderR":"1","shoulderLR":"2","elbow":"3","wrist":"4","finger1":"5","finger2":"6","finger3":"7","finger4":"8","finger5":"9"}
 timeleft = 0.0
 smoothingRate = 10
-lastFrame = 0.0
+lastFrame = time.time()
 while True:
         try:
             # checking for new target values assigned
