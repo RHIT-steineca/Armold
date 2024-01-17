e4ef5ae2ceeaa9e1a0e6609f160e4b808a7035ba
RHIT-steineca <steineca@rose-hulman.edu>
2024-01-17 17:41:27 -0500

    got servos closer to working

--- Code_Files/arduino_testing.py
+++ Code_Files/arduino_testing.py
@@ -11,7 +11,7 @@ connections = {}
 minDegs = {"shoulderCB":0,"shoulderR":0,"shoulderLR":0,"elbow":0,"wrist":0,"finger1":0,"finger2":0,"finger3":0,"finger4":0,"finger5":0}
 maxDegs = {"shoulderCB":270,"shoulderR":2400,"shoulderLR":270,"elbow":93.3,"wrist":150,"finger1":1,"finger2":1,"finger3":1,"finger4":1,"finger5":1}
 minServoVals = {"shoulderCB":0,"shoulderR":0,"shoulderLR":0,"elbow":0,"wrist":0,"finger1":0,"finger2":0,"finger3":0,"finger4":0,"finger5":0}
-maxServoVals = {}
+maxServoVals = {"shoulderCB":180,"shoulderR":180,"shoulderLR":180,"elbow":180, "wrist":180,"finger1":180,"finger2":180,"finger3":180,"finger4":180,"finger5":180}
 maxServoRange = {"shoulderCB":270,"shoulderR":3600,"shoulderLR":270,"elbow":270,"wrist":270,"finger1":1,"finger2":1,"finger3":1,"finger4":1,"finger5":1}
 
 # TODO should set the arduino pin to the new actual value HERE
@@ -21,7 +21,7 @@ def moveArduino(name):
     pin = mapping[name]
     connection = connections[name]
     connection.write(actualVals[name])
-    print(f"{name:11s}@ {pin}: {actualVals[name]:1.1f} = {round(convertValToAngle(name, actualVals[name]),1):3.1f}°")
+    print(f"{name:11s}@ {pin}: {actualVals[name]:1.1f}°")
 
 def convertValToAngle(servoName, servoValue):
     minVal = minServoVals[servoName]
@@ -42,14 +42,16 @@ it.start()
 for name, val in maxServoRange.items():
     if not (name in mapping):
         continue
-    maxServoVals[name] = maxDegs[name] / maxServoRange[name]
+    # maxServoVals[name] = maxDegs[name] / maxServoRange[name]
 for name, val in directions.items():
     if not (name in mapping):
         continue
     range = maxServoVals[name] - minServoVals[name]
     directions[name] = range / 10
     pin = mapping[name]
-    connections[name] = board.get_pin(f'd:{pin}:p')
+    connections[name] = board.get_pin(f'd:{pin}:s')
+    # number bigger for better servo!!!!!
+    board.servo_config(pin, 0, 20000, 0)
 while(True):
     for name, val in actualVals.items():
         if not (name in mapping):
@@ -62,4 +64,4 @@ while(True):
             actualVals[name] = maxServoVals[name]
             directions[name] = directions[name] * -1
         moveArduino(name)
-        time.sleep(0.5)
+        time.sleep(.5)
