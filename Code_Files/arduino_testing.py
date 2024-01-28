ef25e294b83bc30209e99921e7279f2e89b4239e
RHIT-steineca <steineca@rose-hulman.edu>
2024-01-28 21:30:13 +0000

    setup potentiometer reading/conversion in test code

--- Code_Files/arduino_testing.py
+++ Code_Files/arduino_testing.py
@@ -1,9 +1,9 @@
 import os, sys, time, csv, math
 import pyfirmata
+from gpiozero import MCP3008
 
 # set intial robot values
 actualVals = {"shoulderCB":0,"shoulderR":0,"shoulderLR":0,"elbow":0,"wrist":0,"finger1":0,"finger2":0,"finger3":0,"finger4":0,"finger5":0}
-directions = {"shoulderCB":1,"shoulderR":1,"shoulderLR":1,"elbow":1,"wrist":1,"finger1":1,"finger2":1,"finger3":1,"finger4":1,"finger5":1}
 # map of joints to arduino pins
 pinMapping = {"elbow":9}
 connections = {}
@@ -14,11 +14,17 @@ servoMaxRange = {"shoulderCB":270,"shoulderR":3600,"shoulderLR":270,"elbow":270,
 arduinoMinVals = {"shoulderCB":0,"shoulderR":0,"shoulderLR":0,"elbow":0,"wrist":0,"finger1":0,"finger2":0,"finger3":0,"finger4":0,"finger5":0}
 arduinoMaxVals = {"shoulderCB":180,"shoulderR":180,"shoulderLR":180,"elbow":180, "wrist":180,"finger1":180,"finger2":180,"finger3":180,"finger4":180,"finger5":180}
 
+# initialization
+board = pyfirmata.Arduino('/dev/ttyACM0')
+it = pyfirmata.util.Iterator(board)
+it.start()
+knob = MCP3008(0)
+
 def moveArduino(name):
     pin = pinMapping[name]
     connection = connections[name]
     connection.write(actualVals[name])
-    print(f"{name:11s}@ {pin}: {convertValToAngle(name, actualVals[name]):1.1f}°")
+    print(f"knob 0: {round(knob.value, 3):1.3f} -- {name}: {convertValToAngle(name, actualVals[name]):3.1f} deg")
 
 def convertValToAngle(servoName, servoValue):
     minVal = arduinoMinVals[servoName]
@@ -28,24 +34,29 @@ def convertValToAngle(servoName, servoValue):
     valRange = maxVal-minVal
     degRange = maxDeg-minDeg
     percentValue = float((servoValue - minVal) / valRange)
-    degValue = minDeg + (percentValue * degRange)
-    return degValue
-    
-# initialization
-board = pyfirmata.Arduino('/dev/ttyACM0')
-it = pyfirmata.util.Iterator(board)
-it.start()
-for name, val in directions.items():
+    calcAngle = minDeg + (percentValue * degRange)
+    return calcAngle
+
+def convertAngleToVal(servoName, sensorAngle):
+    minVal = arduinoMinVals[servoName]
+    maxVal = arduinoMaxVals[servoName]
+    minDeg = limitedMinDegs[servoName]
+    maxDeg = servoMaxRange[servoName]
+    valRange = maxVal-minVal
+    degRange = maxDeg-minDeg
+    calcVal = sensorAngle * valRange / degRange
+    return calcVal
+
+for name, val in pinMapping.items():
     if not (name in pinMapping):
         continue
-    range = arduinoMaxVals[name] - arduinoMinVals[name]
-    directions[name] = range / 1
+    # directions[name] = arduinoMaxVals[name] - arduinoMinVals[name] / 1
     pin = pinMapping[name]
     connections[name] = board.get_pin(f'd:{pin}:s')
     # # 9g micro servos (180°)
     # board.servo_config(pin, 500, 2430, 0)
     # # 20kg medium servos (270°)
-    # board.servo_config(pin, 500, 2470, 0)
+    board.servo_config(pin, 500, 2470, 0)
     # # 25kg medium servos (270°)
     # board.servo_config(pin, 500, 2490, 0)
     # # 40kg medium servos (270°)
@@ -54,12 +65,6 @@ while(True):
     for name, val in actualVals.items():
         if not (name in pinMapping):
             continue
-        actualVals[name] += directions[name]
-        if(actualVals[name] < arduinoMinVals[name]):
-            actualVals[name] = arduinoMinVals[name]
-            directions[name] = directions[name] * -1
-        elif(actualVals[name] > arduinoMaxVals[name]):
-            actualVals[name] = arduinoMaxVals[name]
-            directions[name] = directions[name] * -1
+        actualVals[name] = convertAngleToVal(name, round(knob.value, 3) * limitedMaxDegs[name])
         moveArduino(name)
-    time.sleep(1.0/1)
+    time.sleep(1.0/60)
