b8b60fe710fc9ada7461b9d5b60ff09543317736
RHIT-steineca <steineca@rose-hulman.edu>
2024-02-09 11:02:09 -0500

    dfgfd

--- Code_Files/test.py
+++ Code_Files/test.py
@@ -9,8 +9,8 @@ def mqtt_callback(type_name, payload):
     #     targetVals[jointName] = float(jointVal)
     # print(f"{keyLine}@{refreshRate}\n{targetVals}")
 
-sending = "Armold/ToBrain"
-receiving = "Armold/ToDummy"
+receiving = "Armold/ToBrain"
+sending = "Armold/ToDummy"
 
 mqtt_client = mqtt_helper.MqttClient()
 mqtt_client.client.clean_session = False
