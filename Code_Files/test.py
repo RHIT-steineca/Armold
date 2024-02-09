dedd6b5ca247f7976ca3b68363c2926d50a16cb3
RHIT-steineca <steineca@rose-hulman.edu>
2024-02-09 10:57:44 -0500

    asdas

--- Code_Files/test.py
+++ Code_Files/test.py
@@ -2,12 +2,12 @@ import mqtt_helper
 
 def mqtt_callback(type_name, payload):
     print(payload)
-    keyLine = payload[0]
-    rateLine = payload[1]
-    refreshRate = float(rateLine)
-    for jointName, jointVal in payload[2]:
-        targetVals[jointName] = float(jointVal)
-    print(f"{keyLine}@{refreshRate}\n{targetVals}")
+    # keyLine = payload[0]
+    # rateLine = payload[1]
+    # refreshRate = float(rateLine)
+    # for jointName, jointVal in payload[2]:
+    #     targetVals[jointName] = float(jointVal)
+    # print(f"{keyLine}@{refreshRate}\n{targetVals}")
 
 sending = "Armold/ToBrain"
 receiving = "Armold/ToDummy"
