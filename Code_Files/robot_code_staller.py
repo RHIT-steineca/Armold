4f73d47644e2ae2ccc880ab8001562c6cf025f86
RHIT-steineca <steineca@rose-hulman.edu>
2024-02-11 19:27:33 -0500

    asds

---
+++ Code_Files/robot_code_staller.py
@@ -0,0 +1,13 @@
+import secrets
+
+overwriteVals = {"shoulderCB":0,"shoulderR":0,"shoulderLR":0,"elbow":0,"wrist":0,"fingerPTR":0,"fingerMDL":0,"fingerRNG":0,"fingerPKY":0,"fingerTHM":0}
+
+valPath = "//home//pi//"
+fullValPath = os.path.join(valPath, "robovals.txt")
+
+while True:
+    with open(fullValPath, "w") as valFile:
+        frameKey = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(math.floor(refreshRate) + 5))
+        robovalString =  f'{str(frameKey)}\n1'
+        for servoname, servoval in overwriteVals.items():
+                robovalString += f'\n"{servoname}",{servoval}'
