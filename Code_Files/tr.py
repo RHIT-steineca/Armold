2fda58d099402d70ecd6409f9bd3f7f9ab59a0fb
RHIT-steineca <steineca@rose-hulman.edu>
2024-02-14 12:31:34 -0500

    asd

--- Code_Files/tr.py
+++ Code_Files/tr.py
@@ -129,7 +129,7 @@ class Connection:
         
     def setup(connection):
         connection.client.callback = lambda type, payload: connection.mqtt_callback(type, payload)
-        connection.client.connect("Armold/ToBrain", "Armold/ToDummy", use_off_campus_broker=False)
+        connection.client.connect("Armold/ToBrain", "Armold/ToDummy", use_off_campus_broker=True)
     
     def mqtt_callback(connection, type_name, payload):
         try:
