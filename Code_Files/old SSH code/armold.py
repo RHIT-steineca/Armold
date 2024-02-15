a0585ee7819b83fd8232ecb958376c66b2cce299
RHIT-steineca <steineca@rose-hulman.edu>
2024-02-15 17:29:00 -0500

    moved old ssh code to new archive folder

--- Code_Files/old SSH code/armold.py
+++ Code_Files/old SSH code/armold.py
@@ -311,7 +311,7 @@ quitCommanded = True
 print("Armold is awake! \nNow looking for its arm...")
 while (quitCommanded):
     try:
-        ssh.connect("ArmoldSecondary", username="pi", password="Armold", timeout=15)
+        ssh.connect("ArmoldSecondary", username="", password="", timeout=15)
         print("\nArm found! Armold is ready to go!")
         while True:
             checkSSHconnection(ssh)
