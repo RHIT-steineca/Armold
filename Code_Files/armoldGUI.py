7c87aa659b36aa92091ef00ba8fea73e1011ba4d
steineca <steineca@rose-hulman.edu>
2024-04-15 19:46:41 -0400

    lots of GUI work

--- Code_Files/armoldGUI.py
+++ Code_Files/armoldGUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import customtkinter as ctk
 from PIL import Image
 from PIL import ImageTk
 import os
@@ -21,47 +22,69 @@ class ArmoldGUI():
 
     def fillWindow(self):
         # recording area frames
-        self.recordingsframeborder = tk.Frame(self.window, width=self.recordingsFrameWidth, highlightbackground="#BABFC9", highlightthickness=5)
+        self.recordingsframeborder = tk.Frame(self.window, width=self.recordingsFrameWidth, background="#FFFFFF", highlightbackground="#BABFC9", highlightthickness=5)
         self.recordingsframeborder.pack(side="left", expand=True, fill="both")
-        self.recordingsframe = tk.Frame(self.recordingsframeborder, background="#BABFC9")
+        self.recordingsframe = ctk.CTkFrame(self.recordingsframeborder, border_color="#BABFC9", border_width=self.borderPadding, corner_radius=15)
         self.recordingsframe.pack(side="left", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
         # recording area elements
-        
+        self.recordingsList = ctk.CTkScrollableFrame(self.recordingsframe, bg_color="#BABFC9", fg_color="#BABFC9", scrollbar_fg_color="#FFFFFF", scrollbar_button_color="#5D6D8B", scrollbar_button_hover_color="#427FF6", corner_radius=15)
+        self.recordingsList.pack(side="bottom", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
         # controls area frames
-        self.controlsframeborder = tk.Frame(self.window, width=self.controlsFrameWidth, highlightbackground="#BABFC9", highlightthickness=5)
+        self.controlsframeborder = tk.Frame(self.window, width=self.controlsFrameWidth, background="#FFFFFF", highlightbackground="#BABFC9", highlightthickness=5)
         self.controlsframeborder.pack(side="right", expand=False, fill="both")
-        canvas = tk.Canvas(self.controlsframeborder, width=self.controlsFrameWidth, height=self.controlsFrameWidth)
+        canvas = tk.Canvas(self.controlsframeborder, width=self.controlsFrameWidth, height=self.controlsFrameWidth, bd=0, highlightthickness=0, background="#FFFFFF")
         canvas.pack(side="top", padx=self.borderPadding, pady=self.borderPadding, expand=False, fill="both")
         img = Image.open("gui_icons/armold_logo_glow.png")
         img = img.resize((round(self.controlsFrameWidth),round(self.controlsFrameWidth)), Image.ANTIALIAS)
         canvas.logo = ImageTk.PhotoImage(img)
         canvas.create_image(0, 0, anchor=tk.NW, image=canvas.logo)
-        self.controlsframe = tk.Frame(self.controlsframeborder, width=self.controlsFrameWidth, background="#BABFC9")
+        self.controlsframe = ctk.CTkFrame(self.controlsframeborder, width=self.controlsFrameWidth, fg_color="#BABFC9", corner_radius=15)
         self.controlsframe.pack(side="bottom", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
         # controls area elements
-        recordButtonFrame = tk.Frame(self.controlsframe, width=self.controlsFrameWidth)
+        recordButtonFrame = ctk.CTkFrame(self.controlsframe, width=self.controlsFrameWidth, fg_color="#FFFFFF", corner_radius=15)
         recordButtonFrame.pack(side="top", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
         self.recordButtonMargin = tk.Frame(recordButtonFrame, width=self.controlsFrameWidth)
         self.recordButtonMargin.pack(side="left", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
-        self.recordButton = tk.Button(self.recordButtonMargin, text="New Recording", command=lambda : self.closeWindow())
+        self.recordButton = ctk.CTkButton(self.recordButtonMargin, text="New Recording", font=("Courier New Bold", 32), text_color="#000000", fg_color="#FFFFFF", bg_color="#FFFFFF", hover_color="#EAF1FF", border_color="#DDDDDD", border_width=self.borderPadding, corner_radius=15, command=lambda : self.startRecording())
         self.recordButton.pack(side="left", expand=True, fill="both")
-        self.recordButtonCanvas = tk.Canvas(recordButtonFrame, width=4+3*self.borderPadding, height=4+3*self.borderPadding)
-        self.recordButtonCanvas.pack(side="right", padx=self.borderPadding, expand=False, fill="none")
-        self.recordButtonCanvas.create_oval(2,2, 3*self.borderPadding, 3*self.borderPadding, fill="#BABFC9", outline="#BABFC9")
+        self.recordButtonCanvas = tk.Canvas(recordButtonFrame, width=3*self.borderPadding, height=3*self.borderPadding, bd=0, highlightthickness=0, background="#FFFFFF")
+        self.recordButtonCanvas.pack(side="right", padx=(self.borderPadding, 2*self.borderPadding), expand=False, fill="none")
+        self.recordButtonCanvas.create_oval(0,0,3*self.borderPadding,3*self.borderPadding, fill="#BABFC9", outline="#FFFFFF")
         
-        self.mirrorButtonFrame = tk.Frame(self.controlsframe, width=self.controlsFrameWidth)
-        self.mirrorButtonFrame.pack(side="top", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
-        self.mirrorButton = tk.Button(self.mirrorButtonFrame, text="Live Control", command=lambda : self.closeWindow())
-        self.mirrorButton.pack(side="left")
-        self.mirrorButtonCanvas = tk.Canvas(self.mirrorButtonFrame, width=4+3*self.borderPadding, height=4+3*self.borderPadding)
-        self.mirrorButtonCanvas.pack(side="right", padx=self.borderPadding, expand=False, fill="none")
-        self.mirrorButtonCanvas.create_oval(2,2, 3*self.borderPadding, 3*self.borderPadding, fill="#BABFC9", outline="#BABFC9")
+        mirrorButtonFrame = ctk.CTkFrame(self.controlsframe, width=self.controlsFrameWidth, fg_color="#FFFFFF", corner_radius=15)
+        mirrorButtonFrame.pack(side="top", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
+        self.mirrorButtonMargin = tk.Frame(mirrorButtonFrame, width=self.controlsFrameWidth)
+        self.mirrorButtonMargin.pack(side="left", padx=self.borderPadding, pady=self.borderPadding, expand=True, fill="both")
+        self.mirrorButton = ctk.CTkButton(self.mirrorButtonMargin, text="Live Control", font=("Courier New Bold", 32), text_color="#000000", fg_color="#FFFFFF", bg_color="#FFFFFF", hover_color="#EAF1FF", border_color="#DDDDDD", border_width=self.borderPadding, corner_radius=15, command=lambda : self.startMirror())
+        self.mirrorButton.pack(side="left", expand=True, fill="both")
+        self.mirrorButtonCanvas = tk.Canvas(mirrorButtonFrame, width=3*self.borderPadding, height=3*self.borderPadding, bd=0, highlightthickness=0, background="#FFFFFF")
+        self.mirrorButtonCanvas.pack(side="right", padx=(self.borderPadding, 2*self.borderPadding), expand=False, fill="none")
+        self.mirrorButtonCanvas.create_oval(0,0,3*self.borderPadding,3*self.borderPadding, fill="#BABFC9", outline="#FFFFFF")
         
-        self.closeButton = tk.Button(self.controlsframe, text="Close Window", command=lambda : self.closeWindow())
-        self.closeButton.pack(side="bottom")
+        tekButtonMargin = ctk.CTkFrame(self.controlsframe, width=self.controlsFrameWidth, fg_color="#BABFC9", corner_radius=15)
+        tekButtonMargin.pack(side="bottom", padx=self.borderPadding, pady=self.borderPadding, expand=False, fill="both")
+        debugButton = ctk.CTkButton(tekButtonMargin, text="Open Debug Window", font=("Courier New Bold", 18), text_color="#000000", fg_color="#FFFFFF", bg_color="transparent", hover_color="#EAF1FF", corner_radius=15, command=lambda : self.showDebugWindow())
+        debugButton.pack(side="left")
+        closeButton = ctk.CTkButton(tekButtonMargin, text="Shut Down", font=("Courier New Bold", 18), text_color="#000000", fg_color="#FFFFFF", bg_color="transparent", hover_color="#EAF1FF", corner_radius=15, command=lambda : self.closeWindow())
+        closeButton.pack(side="right")
 
         self.window.update()
 
+    def startRecording(self):
+        pass
+    
+    def stopRecording(self):
+        pass
+    
+    def startMirror(self):
+        pass
+    
+    def stopMirror(self):
+        pass
+    
+    def showDebugWindow(self):
+        pass
+    
     def closeWindow(self):
         self.window.destroy()
         # os.system("sudo reboot")
